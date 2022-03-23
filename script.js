e3db2bd33293dd76254c973caa92568e165f3e48
alireza babaie <60476541+babaie774@users.noreply.github.com>
2022-03-23 12:07:21 +0430

    put comment in file

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
+//get least value word input
 function getValWord() {
     return document.getElementById('formWord').value;
 }
 
+//get least value textarea input
 function getValText() {
     return document.getElementById('formText').value;
 }
 
-
+//check logic code after onclick
 const formButton = document.getElementById('formButton');
 formButton.addEventListener('click', () => {
     const repeatTime = getValText().split(' ').filter((text) => {
@@ -14,5 +16,4 @@ formButton.addEventListener('click', () => {
     }).length;
     console.log(repeatTime)
     document.getElementById('formResult').innerHTML = `you word repeated ${repeatTime} times`
-})
-
+});
